3fd871be947443390e3271188b81cbe21457dead
JGuan-lab <64457671+JGuan-lab@users.noreply.github.com>
2025-08-05 02:55:51 +0800

    Update example.R
    1. Quik start for cross-validation;
    2. Applying `Row Label` bug fix;
    3. Quick start example added

--- code/example.R
+++ code/example.R
@@ -1,48 +1,92 @@
+## load libraries (install via install.R)
+library(dplyr)
+library(Matrix)
+library(limma)
+library(pheatmap)
+library(edgeR)
+library(scater)
+library(gtools)
+library(reshape2)
+library(data.table)
+library(scales)
+
 ## Source
-source("CDSC.R")
+source("DSSC.R")
 source("function_help.R")
 
 ## Prepare data
-# if one generates simulated bulk data from single-cell data, please use:
-scData <- list(data = readRDS("Segerstolpe.rds"), full_phenoData = readRDS("Segerstolpe_phenoData.rds"))
+# If one generates simulated bulk data from single-cell data, please use:
+scData <- list(data = readRDS("./Segerstolpe.rds"),
+               full_phenoData = readRDS("./Segerstolpe_phenoData.rds"))
 bulkData <- simulation(scData)
 
-# if one analyzes true data, please use:
-bulkData <- trueData 
+# If one analyzes true data, please use:
+#bulkData <- trueData
 # bulkData <- readRDS("CellLines.rds")
 
 # The following code is available if you want to build the bulkData yourself:
 # You can calculate the reference GEP matrix from scData
-C_ref <- scSimulateC(scData,leastNum = 0,plotmarker = F,norm1 = "none",log2.threshold=log2(2))$C
-# or you can alse use your signature
-C_ref <- 'your signature'
-bulkData$Indata <- list(T = 'your bulk data',
-                        C_ref = C_ref,
-                        P = 'your groundtruth')
-# To use CDSC correctly, please ensure that the genes of T and C_ref are the same
+#C_ref <- scSimulateC(scData, leastNum = 0, plotmarker = FALSE, norm1 = "none",
+#                     log2.threshold=log2(2))$C
+# or you can also use your signature
+#C_ref <- 'your signature'
+#bulkData$Indata <- list(T = 'your bulk data',
+#                        C_ref = C_ref,
+#                        P = 'your groundtruth')
+# To use DSSC correctly, please ensure that the genes of T and C_ref are the same
 
 ## Paramater
+
+# Complete parameter adjustment process
+# 
+# para_table <- cross_validation(bulk = bulkData$Indata$T,
+#                                ref = bulkData$Indata$C_ref,
+#                                n_folds = 5,
+#                                seedd = 1234)
+
+
+# =============================Quick cross-validation example for testing DSSC =========================================
+###Time difference of 56.12734 mins
 para_table <- cross_validation(bulk = bulkData$Indata$T,
                                 ref = bulkData$Indata$C_ref,
-                                n_folds = 5,
-                                seedd = 1234)
+                                n_folds = 2,
+                                seedd = 1234,lambda1 = c(0,0.001),lambda2 = c(0,0.001),lambdaC = c(1000,0))
 #you can chose other strategy, "which.min(para_table$RMSE.T)" especially when performing complete deconvolution
 lambda1 <- para_table$lambda1[which.max(para_table$PCC.C)]
 lambda2 <- para_table$lambda2[which.max(para_table$PCC.C)]
 lambdaC <- para_table$lambdaC[which.max(para_table$PCC.C)]
 
 ## Deconvolution
-retult <- CDSC(data_bulk = bulkData$Indata$T, 
+result <- DSSC(data_bulk = bulkData$Indata$T,
                data_ref = bulkData$Indata$C_ref,
-               lambda1 = lambda1, 
+               lambda1 = lambda1,
                lambda2 = lambda2,
                lambdaC = lambdaC,
-               Ss = SM(t(scData$Indata$T)),
-               Sg = SM(scData$Indata$T))
-ctlabels <- Row_label(bulkData$Indata$C_ref,retult$c,leastnum = 3)
-rownames(retult$p) <- ctlabels
-colnames(retult$c) <- ctlabels
+               Ss = SM(t(bulkData$Indata$T)),
+               Sg = SM(bulkData$Indata$T))
+
+# ===================================================================================================================
+# =============================Quick start for DSSC deconvolution====================================================
+
+
+result <- DSSC(data_bulk = bulkData$Indata$T,
+               data_ref = bulkData$Indata$C_ref,
+               lambda1 = 0,
+               lambda2 = 0,
+               lambdaC = 1000,
+               Ss = SM(t(bulkData$Indata$T)),
+               Sg = SM(bulkData$Indata$T))
+
+
+# =================================================================================================================
+
+
+
+ctlabels <- Row_label(result$c,bulkData$Indata$C_ref, leastnum = 3)
+rownames(result$p) <- ctlabels
+colnames(result$c) <- ctlabels
 
 ## Evalution
-# please ensure consistency in cell type
-getPearsonRMSE(retult$p, bulkData$Indata$P)
+# Please ensure consistency in cell type
+getPearsonRMSE(result$p, bulkData$Indata$P)
+getPearsonRMSE(result$c, bulkData$Indata$C)
